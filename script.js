1f1b32a5346518cd1ce6c9ee9907d1162313dc61
maxcorni <maxcorni55@gmail.com>
2025-03-20 13:24:53 +0100

    feat(js): js modification add filter button for json card creation

---
+++ script.js
@@ -0,0 +1,59 @@
+let donnees = [];
+const liste = document.getElementById('produitsList');
+const h1 = document.querySelector('h1');  // Récupérer le <h1>
+const burgersButton = document.getElementById('burgersButton');
+const sidesButton = document.getElementById("sidesButton");
+const drinksButton = document.getElementById("drinksButton");
+const dessertsButton = document.getElementById("dessertsButton");
+const menusButton = document.getElementById("menusButton");
+const happymealButton = document.getElementById("happymealButton");
+
+// Charger les données JSON
+fetch('../mcdo.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        donnees = data;
+        burgersButton.addEventListener('click', () => afficherProduits('burgers', 'Burgers'));
+        sidesButton.addEventListener('click', () => afficherProduits('sides', 'Accompagnements'));
+        drinksButton.addEventListener('click', () => afficherProduits('drinks', 'Boissons'));
+        dessertsButton.addEventListener('click', () => afficherProduits('desserts', 'Desserts'));
+        menusButton.addEventListener('click', () => afficherProduits('menus', 'Menus'));
+        happymealButton.addEventListener('click', () => afficherProduits('happyMeal', 'Happy Meal'));
+    })
+    .catch(function(error) {
+        console.error("Erreur lors du chargement du fichier JSON : ", error);
+    });
+
+// Fonction générique pour afficher les produits
+function afficherProduits(type, titre) {
+    // Vider la liste des produits précédemment affichés
+    liste.innerHTML = "";
+
+    // Mettre à jour le titre <h1>
+    h1.textContent = titre;
+
+    // Obtenir les produits à partir de l'objet 'donnees' en fonction du type
+    const produits = donnees[type];
+
+    // Vérifier si des produits existent pour le type donné
+    if (produits) {
+        produits.forEach(function(produit) {
+            const produitHTML = `
+                <div class="produit-card">
+                    <a href="#">
+                        <img src="../assets/${produit.image}" alt="${produit.name}">
+                    </a>
+                    <div class="produit-info">
+                        <h2>${produit.name}</h2>
+                        <p>${produit.price} €</p>
+                    </div>
+                </div>
+            `;
+
+            // Ajouter le HTML généré à la liste
+            liste.innerHTML += produitHTML;
+        });
+    }
+}
